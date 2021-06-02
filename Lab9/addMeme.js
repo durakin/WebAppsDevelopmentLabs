4511815732b5d83cdac5520a4a345ce08eed2740
Nepomniashchii <albertnepomnyashiy@gmail.com>
2021-06-02 22:46:02 +0700

    Lab9 done

--- Lab9/addMeme.js
+++ Lab9/addMeme.js
@@ -1,12 +1,18 @@
-function addMemeAction() {
+function addMemeAction(event) {
+    event.preventDefault();
+    if (document.getElementById("meme").value === "" ||
+    document.getElementById("description").value === "") {
+        console.log("Nothing to add");
+        return;
+    }
+
     console.log("addMeme() called")
-    let list_of_memes = document.getElementById("list_of_memes");
     console.log(list_of_memes)
     let new_entry = {
         header : document.createElement("h3"),
         paragraph : document.createElement("p")
     };
-    new_entry.header.innerHTML = "SSAO <a id="+document.getElementById("meme").value+"></a>";
+    new_entry.header.innerHTML = document.getElementById("meme").value + "<a id="+document.getElementById("meme").value+"></a>";
     new_entry.paragraph.innerText = document.getElementById("description").value;
     list_of_memes.appendChild(new_entry.header);
     list_of_memes.appendChild(new_entry.paragraph);
